26e35151287b0f3d16553ca8a6f2e850e2cc417b
vasile <elsile69@yahoo.com>
2018-10-09 23:33:52 +0300

    Change font to Fantasque

--- config.def.h
+++ config.def.h
@@ -5,7 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "DejaVu:pixelsize=14:antialias=true:hinting=true:hintstyle=hintfull";
+/* static char font[] = "DejaVu:pixelsize=14:antialias=true:hinting=true:hintstyle=hintfull"; */
+static char font[] = "FantasqueSansMono:pixelsize=18:antialias=false:autohint=false";
 static int borderpx = 2;
 
 /*
