2a17edd8eb247be6fb3674692e2cf15b1be5902a
vasile <elsile69@yahoo.com>
2018-10-09 23:23:17 +0300

    Change font to DejaVu

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "Monospace:pixelsize=14:antialias=true:hinting=true:hintstyle=hintfull";
+static char font[] = "DejaVu:pixelsize=14:antialias=true:hinting=true:hintstyle=hintfull";
 static int borderpx = 2;
 
 /*
