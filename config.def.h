a18f2f0663ca87972ad3cbabc1a31f82dae346db
vasile <elsile69@yahoo.com>
2018-10-09 23:16:10 +0300

    I hope it's opaque

--- config.def.h
+++ config.def.h
@@ -11,7 +11,7 @@ static int borderpx = 2;
 /*
  * terminal transparency
  */
-static uint8_t term_alpha = 0xdd;
+static uint8_t term_alpha = 0xff;
 
 /*
  * What program is execed by st depends of these precedence rules:
